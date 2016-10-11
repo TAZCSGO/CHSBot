752e9706a528a9b5a62d0d45911a90ec82e97cf7
TGWaffles <thomas.wilcox@hotmail.co.uk>
2016-10-11 18:21:23 +0100

    Start of bot.

---
+++ bot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import praw
+
+user_agent = ("CryoBot 1.0")
+
+r = praw.Reddit(user_agent = user_agent)
+
+subreddit = r.get_subreddit("chscomputing")
+
+for submission in subreddit.get_new(limit = 5):
+	print "Title: ", submission.title
+	print "Text: ", submission.selftext
+	print "Score: ", submission.score
+	print "Author: ", submission.author
+	print "---------------------------------\n
